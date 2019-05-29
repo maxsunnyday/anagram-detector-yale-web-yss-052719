bbb2798eea758b89230f673979f1220e7d4bccb5
maxsunnyday <max.sun@yale.edu>
2019-05-29 13:20:03 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
 # Your code goes here!
+class Anagram
+    attr_accessor :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(anagrams)
+        letters = word.split("")
+        anagrams.select do |possible|
+            possible.split("").sort == letters.sort
+        end
+    end
+end
